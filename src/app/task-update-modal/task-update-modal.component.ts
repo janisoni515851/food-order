7564abb5bc40f5d3842f372bf71a9327c21934c3
js <janvisoni515851@gmail.com>
2025-06-13 20:00:36 +0530

    task update modal changes

--- src/app/task-update-modal/task-update-modal.component.ts
+++ src/app/task-update-modal/task-update-modal.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { DUMMY_TECHS } from '../dummy-users';
 
 @Component({
@@ -9,23 +16,24 @@ import { DUMMY_TECHS } from '../dummy-users';
 export class TaskUpdateModalComponent implements OnChanges {
   @Input() selectedInfo: any = {};
   @Output() onUserUpdate = new EventEmitter<any>();
-  selectedUser: any = {};
   techs = DUMMY_TECHS;
   techId: number | null = null;
+  selectedUser: any = {};
 
-  // constructor(){
-  //   // this.techId = this.selectedInfo.techId;
-  //   console.log(this.selectedInfo, this.techId);
-  // }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    this.selectedUser = {...this.selectedInfo};
+  constructor() {
+    // this.techId = this.selectedInfo.techId;
   }
 
-  updateUser(){
-    console.log("selectedUser", this.selectedUser);
-    if(typeof this.selectedUser.techId === 'string') {
-      this.selectedUser.techId = parseInt(this.selectedUser.techId, 10);
+  ngOnChanges(changes: SimpleChanges): void {
+    // this.techId = this.selectedInfo.techId;
+    // console.log(this.selectedInfo, this.techId);
+    this.selectedUser = { ...this.selectedInfo }; //here new object is create and it passes to the selectedUser
+  }
+
+  updateUser() {
+    console.log('selectedinfo--------', this.selectedInfo);
+    if (typeof this.selectedUser.techId === 'string') {
+      this.selectedUser.techId = parseInt(this.selectedUser.techId);
     }
     this.onUserUpdate.emit(this.selectedUser);
   }
